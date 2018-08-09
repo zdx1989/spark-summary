a17927a0abecc53c2563fccae1797499f5b3a03c
zdx1989 <18565653302@163.com>
2018-08-09 22:53:06 +0800

    spark SQL framework

---
+++ src/main/scala/sparkSQL/SQLFramework.scala
@@ -0,0 +1,18 @@
+package sparkSQL
+
+/**
+  * Created by zhoudunxiong on 2018/8/9.
+  */
+object SQLFramework {
+
+  def main(args: Array[String]): Unit = {
+    //spark SQL主要由Core、Catalyst、Hive、Hive-thriftServer组成
+    //core: 负责处理数据的输入和输出，从不同的数据源中获取数据，将查询结果输出为dataFrame
+    //catalyst:负责整个查询语句处理过程，包括解析、绑定、优化、物理执行计划
+    //hive：对hive的数据进行处理
+    //hive-thriftServe：提供CLI和JDBC／ODBC的接口
+
+    
+  }
+
+}
